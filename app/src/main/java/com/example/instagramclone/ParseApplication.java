24688d23e51409811ad3ae124b40f48417e4b675
minaguirguis <mguirguis.10@gmail.com>
2021-03-02 21:44:51 -0500

    Finished video

--- app/src/main/java/com/example/instagramclone/ParseApplication.java
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -3,12 +3,17 @@ package com.example.instagramclone;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+
+        ParseObject.registerSubclass(Post.class);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("Q6Ls6sb8X0udRA8M4ezS5weTBNXo3xpHin5mKBaT")
                 .clientKey("sD94fUeqdjoq3RJtciaWW7uWZg4fvakT9ERa21aJ")
